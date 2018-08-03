dedfc217b3ce63a2dbbffbffaa18a9a9cf0aef4f
Thomas Sankey <41643165+Tom8909@users.noreply.github.com>
2018-08-03 15:03:03 +0100

    Add files via upload

---
+++ JS -1/ageCalc.js
@@ -0,0 +1,3 @@
+var age = prompt("How old are you?");
+var days = age*365.25; //.25 for quarter of years
+alert("You've been alive for: " + days);
---
+++ JS -1/index.js
@@ -0,0 +1,10 @@
+var firstName = prompt("Please enter first name; ");
+var lastName = prompt("Please enter surname: ");
+var age = prompt("Please enter your age: ");
+
+var fullName = firstName + " " + lastName;
+
+alert("Hello there " + fullName);
+alert("Your age is " + age);
+console.log("Hello again " + fullName);
+console.log("Your age again is " + age);
